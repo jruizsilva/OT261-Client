3de2c85bcf25e6a2b70cfdc025573414331fde20
jruizsilva <ruizsilvajonathan@gmail.com>
2022-08-18 09:33:44 -0300

    Backoffice layout finished

--- src/Components/BackofficeLayout/BackofficeLayout.js
+++ src/Components/BackofficeLayout/BackofficeLayout.js
@@ -7,7 +7,7 @@ const cardList = [
   { id: 1, title: 'Novedades', image: icons.news },
   { id: 2, title: 'Actividades', image: icons.activities },
   { id: 3, title: 'Categorias', image: icons.categories },
-  { id: 4, title: 'Testimonios', image: icons.feedback },
+  { id: 4, title: 'Testimonios', image: icons.testimonials },
   { id: 5, title: 'Organización', image: icons.organization },
   { id: 6, title: 'Slides', image: icons.slides },
   { id: 7, title: 'Usuarios', image: icons.users },
@@ -23,9 +23,16 @@ export const BackofficeLayout = () => {
       <main className={style.backoffice__main}>
         <ul className={style.backoffice__ul}>
           {cardList.map(({ title, image, id }) => (
-            <li key={id}>
-              <h2>{title}</h2>
-              <img src={image} alt={title} />
+            <li key={id} className={style.backoffice__li}>
+              <h2 className={style.backoffice__h2}>{title}</h2>
+              <div className={style['backoffice__image-container']}>
+                <img
+                  src={image}
+                  alt={title}
+                  className={style.backoffice__image}
+                />
+              </div>
+              <button className={style.backoffice__button}>Ir</button>
             </li>
           ))}
         </ul>
--- src/Components/BackofficeLayout/assets/index.js
+++ src/Components/BackofficeLayout/assets/index.js
@@ -1,6 +1,6 @@
 import activities from './activities.svg';
 import categories from './categories.svg';
-import feedback from './feedback.svg';
+import testimonials from './testimonials.svg';
 import members from './members.svg';
 import news from './news.svg';
 import organization from './organization.svg';
@@ -11,7 +11,7 @@ export const icons = {
   news,
   activities,
   categories,
-  feedback,
+  testimonials,
   members,
   organization,
   slides,
