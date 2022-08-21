942ebe7d068d83399ea4472345d7bf06209f46fb
jruizsilva <ruizsilvajonathan@gmail.com>
2022-08-20 22:41:53 -0300

    file consts added and import consts in backoffice component

---
+++ src/screen/backoffice/consts.js
@@ -0,0 +1,14 @@
+const adminCardList = [
+  { id: 1, title: 'Novedades', image: icons.news },
+  { id: 2, title: 'Actividades', image: icons.activities },
+  { id: 3, title: 'Categorias', image: icons.categories },
+  { id: 4, title: 'Testimonios', image: icons.testimonials },
+  { id: 5, title: 'Organización', image: icons.organization },
+  { id: 6, title: 'Slides', image: icons.slides },
+  { id: 7, title: 'Usuarios', image: icons.users },
+  { id: 8, title: 'Miembros', image: icons.members },
+  { id: 9, title: 'Editar perfil', image: icons.edit },
+];
+const userCardList = [{ id: 1, title: 'Editar perfil', image: icons.edit }];
+
+export { adminCardList, userCardList };
--- src/screen/backoffice/index.js
+++ src/screen/backoffice/index.js
@@ -15,18 +15,7 @@ import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
-const adminCardList = [
-  { id: 1, title: 'Novedades', image: icons.news },
-  { id: 2, title: 'Actividades', image: icons.activities },
-  { id: 3, title: 'Categorias', image: icons.categories },
-  { id: 4, title: 'Testimonios', image: icons.testimonials },
-  { id: 5, title: 'Organización', image: icons.organization },
-  { id: 6, title: 'Slides', image: icons.slides },
-  { id: 7, title: 'Usuarios', image: icons.users },
-  { id: 8, title: 'Miembros', image: icons.members },
-  { id: 9, title: 'Editar perfil', image: icons.edit },
-];
-const userCardList = [{ id: 1, title: 'Editar perfil', image: icons.edit }];
+import { adminCardList, userCardList } from './consts';
 
 const StyledContainer = styled(Stack)`
   background-color: #edf2f7;
