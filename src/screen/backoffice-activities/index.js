4e58fbd9d27a6b3a27d04c6b97f735a6eafcda09
jruizsilva <ruizsilvajonathan@gmail.com>
2022-08-24 12:39:05 -0300

    open modal on edit click

--- src/screen/backoffice-activities/index.js
+++ src/screen/backoffice-activities/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Button, Container, Stack, Table } from 'react-bootstrap';
+import { Button, Container, Stack, Table, Form, Modal } from 'react-bootstrap';
+
 import Swal from 'sweetalert2';
 import { StyledContainer } from './styles';
 import { deleteActivityAsync } from '../../store/slice/activities';
@@ -8,10 +9,13 @@ import { deleteActivityAsync } from '../../store/slice/activities';
 export const BackofficeActivities = () => {
   const { activities } = useSelector((state) => state.activities);
   const dispatch = useDispatch();
+  const [show, setShow] = useState(false);
 
-  console.log(activities);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const handleEditClick = (id) => {
+    handleShow();
     const activityToEdit = activities.find((activity) => activity.id === id);
     console.log(activityToEdit);
   };
@@ -34,45 +38,72 @@ export const BackofficeActivities = () => {
   };
 
   return (
-    <StyledContainer className='pt-5'>
-      <Stack as='main'>
-        <Container>
-          <h2 className='h2 text-center mb-4'>Lista de actividades</h2>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th className='text-center'>#</th>
-                <th className='text-center'>Titulo</th>
-                <th className='text-center'>Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {activities.map(({ id, title }) => (
-                <tr key={id}>
-                  <td className='text-center'>{id}</td>
-                  <td className='text-center'>{title}</td>
-                  <td className='text-center d-flex justify-content-center gap-2 '>
-                    <Button
-                      variant='warning'
-                      size='sm'
-                      onClick={() => handleEditClick(id)}
-                    >
-                      Editar
-                    </Button>
-                    <Button
-                      variant='danger'
-                      size='sm'
-                      onClick={() => handleDeleteClick(id)}
-                    >
-                      Eliminar
-                    </Button>
-                  </td>
+    <>
+      <StyledContainer className='pt-5'>
+        <Stack as='main'>
+          <Container>
+            <h2 className='h2 text-center mb-4'>Lista de actividades</h2>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th className='text-center'>#</th>
+                  <th className='text-center'>Titulo</th>
+                  <th className='text-center'>Acciones</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
-        </Container>
-      </Stack>
-    </StyledContainer>
+              </thead>
+              <tbody>
+                {activities.map(({ id, title }) => (
+                  <tr key={id}>
+                    <td className='text-center'>{id}</td>
+                    <td className='text-center'>{title}</td>
+                    <td className='text-center d-flex justify-content-center gap-2 '>
+                      <Button
+                        variant='warning'
+                        size='sm'
+                        onClick={() => handleEditClick(id)}
+                      >
+                        Editar
+                      </Button>
+                      <Button
+                        variant='danger'
+                        size='sm'
+                        onClick={() => handleDeleteClick(id)}
+                      >
+                        Eliminar
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Container>
+        </Stack>
+      </StyledContainer>
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Editar actividad</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+              <Form.Label>Titulo</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Agregue el titulo de la actividad'
+                autoFocus
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleClose}>
+            Cerrar
+          </Button>
+          <Button variant='primary' onClick={handleClose}>
+            Guardar cambios
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
