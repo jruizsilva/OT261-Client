6d686d444f27244a9c78110aa3a494381ad02dde
jruizsilva <ruizsilvajonathan@gmail.com>
2022-08-24 14:07:52 -0300

    formik added and using formik in ModalEditActivity

--- src/Components/ModalEditActivity/ModalEditActivity.js
+++ src/Components/ModalEditActivity/ModalEditActivity.js
@@ -1,32 +1,47 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Form, Modal, Button } from 'react-bootstrap';
+import { useFormik } from 'formik';
 
 export const ModalEditActivity = ({ show, handleClose }) => {
+  const { activityToEdit } = useSelector((state) => state.activities);
+
+  const formik = useFormik({
+    initialValues: {
+      title: activityToEdit.title,
+    },
+    onSubmit: () => {
+      console.log(formik.values);
+    },
+  });
   return (
     <Modal show={show} onHide={handleClose} centered>
-      <Modal.Header closeButton>
-        <Modal.Title>Editar actividad</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
+      <Form onSubmit={formik.handleSubmit}>
+        <Modal.Header closeButton>
+          <Modal.Title>Editar actividad</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
           <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
             <Form.Label>Titulo</Form.Label>
             <Form.Control
               type='text'
               placeholder='Actualiza el titulo de la actividad'
               autoFocus
+              name='title'
+              onChange={formik.handleChange}
+              value={formik.values.title}
             />
           </Form.Group>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant='secondary' onClick={handleClose}>
-          Cerrar
-        </Button>
-        <Button variant='primary' onClick={handleClose}>
-          Guardar cambios
-        </Button>
-      </Modal.Footer>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleClose}>
+            Cerrar
+          </Button>
+          <Button type='submit' variant='primary'>
+            Guardar cambios
+          </Button>
+        </Modal.Footer>
+      </Form>
     </Modal>
   );
 };
