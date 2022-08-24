b28dd00f97fac040289378825237ff956481dbbf
patxi <patxiarana05@gmail.com>
2022-08-24 11:56:01 -0300

    login/regiter created

---
+++ src/Components/login/Login.js
@@ -0,0 +1,52 @@
+import React from "react";
+import {Button, Col, Container, Form, Row} from "react-bootstrap";
+import { Link } from 'react-router-dom';
+
+import './login.css'
+
+const img = {
+    img:'https://cdn.pixabay.com/photo/2021/03/12/08/51/shorturl-6089108_960_720.jpg'
+}
+
+const Login = () =>{
+
+return(
+
+<>        
+  <div className="body">
+     <img src={img.img} className='img'/>
+        <h2 className="welcome">Bienvenido</h2>
+            <h1 className="login">Inicia sesión en tu cuenta!</h1>
+            <Container className="loginbody">
+                <Row className="mt-5">
+                    <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
+                        <Form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="email" />
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <div className="d-grid gap-2">
+                           <Button variant="danger" size="lg">
+                           Inicia sesión
+                           </Button>
+                           </div>
+                        </Form>
+                    </Col>
+                </Row>
+                <h6 className="mt-5 p-5 text-center text-secondary ">No tienes cuenta ?<Link to="/" className="link">Registrate</Link> </h6>
+            </Container>
+            </div>
+        </>
+
+)
+
+}
+
+
+
+export default Login;
--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,16 @@ import { Route, Routes } from 'react-router-dom';
 import Home from '../screen/home';
 import News from '../screen/news';
 import Backoffice from '../screen/backoffice';
-
+import Login from '../Components/login/Login';
+import Registration from '../Components/Registration/Registration';
 const Routing = () => {
   return (
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/news' element={<News />} />
         <Route path='/backoffice' element={<Backoffice />} />
+        <Route path='/login' element={<Login />} />v
+        <Route path='/register' element={< Registration />} />
       </Routes>
   );
 };
