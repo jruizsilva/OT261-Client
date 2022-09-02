c82db535699fc1d2c034b6165d2e156c1b3c4d67
jruizsilva <ruizsilvajonathan@gmail.com>
2022-09-02 17:23:39 -0300

    const added

--- src/store/slice/activities.js
+++ src/store/slice/activities.js
@@ -1,29 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 import Swal from 'sweetalert2'
-
-/* ======================
-   HARDCODEO las actividades ↓↓ (deberia venir de la db)
-   ====================== */
-const listActivities = [
-  {
-    id: 1,
-    title: 'Actividad 1',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nihil doloribus qui assumenda facere voluptatibus voluptate sapiente vel et voluptatum. 1'
-  },
-  {
-    id: 2,
-    title: 'Actividad 2',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nihil doloribus qui assumenda facere voluptatibus voluptate sapiente vel et voluptatum. 2'
-  },
-  {
-    id: 3,
-    title: 'Actividad 3',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nihil doloribus qui assumenda facere voluptatibus voluptate sapiente vel et voluptatum. 3'
-  }
-]
+import { listActivities } from './const'
 
 export const activitiesSlice = createSlice({
   name: 'activities',
