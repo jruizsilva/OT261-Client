8b6d258b62c3ccc45e072d2a52dc6882a43be36a
florMignini <flormignini29@gmail.com>
2022-09-05 18:50:14 -0300

    form styles

--- src/Components/Testimonials/TestimonialsForm.jsx
+++ src/Components/Testimonials/TestimonialsForm.jsx
@@ -2,55 +2,98 @@ import * as Yup from "yup";
 import { useFormik } from "formik";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import "../FormStyles.css";
+// import "../FormStyles.css";
+import {
+  Container,
+  FromWrapper,
+  Label,
+  Form,
+  FromGroup,
+  ErrorMessage,
+  Button,
+  TitleContainer,
+  Title,
+  Input,
+} from "./TestimonialsStyle.js";
 
 const validationSchema = Yup.object({
   name: Yup.string().required("name must be provided"),
-  description: Yup.string().required("description must be provided"),
+  description: Yup.string().max(300).required("description must be provided"),
 });
 
-const onSubmit = () => {
-  console.log(`submitted!`);
+const onSubmit = (values, actions) => {
+  console.log(values);
+  console.log(actions);
+  actions.resetForm();
 };
 
 const TestimonialForm = () => {
-  const { values, errors, handleChange, handleSubmit, setFieldValue } =
-    useFormik({
-      initialValues: {
-        name: "",
-        description: "",
-      },
-      validationSchema,
-      onSubmit,
-    });
-  console.log(errors);
+  const {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleSubmit,
+    isSubmitting,
+    setFieldValue,
+  } = useFormik({
+    initialValues: {
+      name: "",
+      description: "",
+    },
+    validationSchema,
+    onSubmit,
+  });
+
   const inputHandler = (event, editor) => {
-    // console.log(event);
     setFieldValue("description", editor.getData());
   };
+  console.log(errors);
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <label htmlFor="name">name</label>
-      <input
-        value={values.name}
-        onChange={handleChange}
-        id="name"
-        type="name"
-        placeholder="Enter your name"
-      />
-      <label htmlFor="description">description</label>
-      <CKEditor
-        className="inputText"
-        editor={ClassicEditor}
-        onChange={inputHandler}
-        id="description"
-        type="description"
-        placeholder="Enter your description"
-      />
-      <button className="submit-btn" type="submit">
-        Send
-      </button>
-    </form>
+    <Container>
+      <FromWrapper>
+        <Form className="form-container" onSubmit={handleSubmit}>
+          <TitleContainer>
+            <Title>Create a Testimonial</Title>
+          </TitleContainer>
+          {isSubmitting ? <p id="success-message"></p> : null}
+          <FromGroup>
+            <Label htmlFor="name">Name</Label>
+            <Input
+              value={values.name}
+              onChange={handleChange}
+              id="name"
+              type="name"
+              placeholder="Enter your name"
+              className={errors.name && touched.name ? "input-error" : ""}
+            />
+            {errors.name && touched.name && (
+              <ErrorMessage>{errors.name}</ErrorMessage>
+            )}
+          </FromGroup>
+          <FromGroup>
+            <Label htmlFor="description">Description</Label>
+            <CKEditor
+              className={
+                errors.description && touched.description ? "input-error" : ""
+              }
+              editor={ClassicEditor}
+              onChange={inputHandler}
+              id="description"
+              type="description"
+              placeholder="Enter your description"
+            />
+            {errors.description && touched.description && (
+              <ErrorMessage>{errors.description}</ErrorMessage>
+            )}
+          </FromGroup>
+
+          <Button className="submit-btn" type="submit">
+            Send Testimonial
+          </Button>
+        </Form>
+      </FromWrapper>
+    </Container>
   );
 };
 
