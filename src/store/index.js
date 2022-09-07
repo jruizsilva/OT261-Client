fde2a8cba2171f4ef7279c5456feabca72fefbda
patxi <patxiarana05@gmail.com>
2022-09-07 17:24:56 -0300

    back office testimonials created

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './slice/counter';
 import userReducer from './slice/user';
 import activitiesReducer from './slice/activities';
-
+import testimonialsReducer from './slice/testimonials';
 export default configureStore({
   reducer: {
     counter: counterReducer,
     activities: activitiesReducer,
     user: userReducer,
+    testimonials:testimonialsReducer
   },
 });
+
+
+
