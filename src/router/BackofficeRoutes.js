176971b918e6ffb3f391b3504320728dde0d119c
florMignini <flormignini29@gmail.com>
2022-09-09 14:25:44 -0300

    index deleted

--- src/router/BackofficeRoutes.js
+++ src/router/BackofficeRoutes.js
@@ -4,7 +4,7 @@ import Backoffice from "../screen/backoffice";
 import { RequireAuth } from "./RequireAuth";
 import { RequireAdminRole } from "./RequireAdminRole";
 import { BackofficeActivities } from "../screen/backoffice-activities";
-import { BackofficeTestimonials } from "../screen/backoffice-testimonials/index,";
+import BackofficeTestimonials from "../screen/backoffice-testimonials";
 
 export const BackofficeRoutes = () => {
   return (
