e87cfcc7ee3c85906881b60f3f07bba89ba6ae04
jruizsilva <ruizsilvajonathan@gmail.com>
2022-09-15 11:07:21 -0300

    useAuth added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Container } from 'react-bootstrap'
 
 import NavBar from './Components/NavBar/NavBar'
@@ -8,26 +8,10 @@ import Routing from './router'
 import './App.css'
 import Footer from './Components/Footer/Footer'
 import NavBar2 from './Components/NavBar2/NavBar2'
-import { useState } from 'react'
-import { httpAxiosInstance } from './Services/httpAxiosInstance'
+import { useAuth } from './hooks/useAuth'
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem('token'))
-  const [loading, setLoading] = useState(false)
-  const { user } = useSelector(state => state.user)
-
-  useEffect(() => {
-    if (token && !user) {
-      httpAxiosInstance
-        .get('/auth')
-        .then(response => {
-          console.dir(response)
-        })
-        .catch(err => {
-          console.dir(err)
-        })
-    }
-  }, [token, user])
+  const { loading } = useAuth()
 
   return (
     <>
